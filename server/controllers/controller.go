c9a05254dd6791784d8aaad0637f67ce6398b2f3
PotterHead-nc0 <rohitstewart987@gmail.com>
2024-07-21 16:09:38 +0530

    code refactor

--- server/controllers/controller.go
+++ server/controllers/controller.go
@@ -6,47 +6,12 @@ import (
 	"io"
 	"net/http"
 	"server/config"
+	"server/interfaces"
 )
-
-type ROI struct {
-	Times      float64 `json:"times"`
-	Currency   string  `json:"currency"`
-	Percentage float64 `json:"percentage"`
-}
-
-type Coin struct {
-	ID                        string  `json:"id"`
-	Symbol                    string  `json:"symbol"`
-	Name                      string  `json:"name"`
-	Image                     string  `json:"image"`
-	CurrentPrice              float64 `json:"current_price"`
-	MarketCap                 float64 `json:"market_cap"`
-	MarketCapRank             int     `json:"market_cap_rank"`
-	FullyDilutedValuation     float64 `json:"fully_diluted_valuation"`
-	TotalVolume               float64 `json:"total_volume"`
-	High24h                   float64 `json:"high_24h"`
-	Low24h                    float64 `json:"low_24h"`
-	PriceChange24h            float64 `json:"price_change_24h"`
-	PriceChangePercentage24h  float64 `json:"price_change_percentage_24h"`
-	MarketCapChange24h        float64 `json:"market_cap_change_24h"`
-	MarketCapChangePercentage float64 `json:"market_cap_change_percentage_24h"`
-	CirculatingSupply         float64 `json:"circulating_supply"`
-	TotalSupply               float64 `json:"total_supply"`
-	MaxSupply                 float64 `json:"max_supply"`
-	Ath                       float64 `json:"ath"`
-	AthChangePercentage       float64 `json:"ath_change_percentage"`
-	AthDate                   string  `json:"ath_date"`
-	Atl                       float64 `json:"atl"`
-	AtlChangePercentage       float64 `json:"atl_change_percentage"`
-	AtlDate                   string  `json:"atl_date"`
-	ROI                       *ROI    `json:"roi"`
-	LastUpdated               string  `json:"last_updated"`
-}
 
 var apiKey = config.GetEnvValue("API_KEY")
 
-func getCoinData() ([]Coin, error) {
-	fmt.Println("chutki")
+func getCoinData() ([]interfaces.Coin, error) {
 
 	url := "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=volume_desc&per_page=1000&page=1&sparkline=false"
 
@@ -63,7 +28,7 @@ func getCoinData() ([]Coin, error) {
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
 
-	var coins []Coin
+	var coins []interfaces.Coin
 	err := json.Unmarshal(body, &coins)
 	if err != nil {
 		return nil, err
