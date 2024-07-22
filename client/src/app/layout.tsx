9c23d776ce347bdf0ec4bd161bc7e9fed3a4f1d9
PotterHead-nc0 <rohitstewart987@gmail.com>
2024-07-23 02:27:48 +0530

    minor change

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cryptxc",
+  title: "Cryptix",
   description: "Crypto Tracker Web App",
 };
 
